dcd42a178a01e14011c6d4c937710cf513673477
tarciosilva <tarciopereirasilva@outlook.com>
2024-02-25 21:04:44 -0300

    add components

---
+++ angular-blog/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuTitleComponent,
+    MenuBarComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
